40018eade708c37d3fb664147fa489ff1040ef04
Kejoh <kejoh12@gmail.com>
2015-05-19 17:31:59 +0100

    ionic base eslem

--- www/app/products/products.controller.js
+++ www/app/products/products.controller.js
@@ -2,10 +2,10 @@
 angular.module('starter.controllers')
 
 .controller('ProductsCtrl', function($scope) {
-  var cat = new Product('Cat', 'assets/img/cat.jpg', 'Lorem ipsum dolor');
-  var skyrim = new Product('Skyrim', 'assets/img/skyrim.jpg', 'Lorem ipsum dolor');
-  var ninja = new Product('Ninja', 'assets/img/white.jpg', 'Lorem ipsum dolor');
-  $scope.products = [skyrim, cat, ninja];
+  var bistec = new Product('bistec', 'assets/img/bistec.jpg', 'Lorem ipsum dolor');
+  var careta = new Product('careta', 'assets/img/careta.jpg', 'Lorem ipsum dolor');
+  var chorizo = new Product('chorizo', 'assets/img/chorizo.jpg', 'Lorem ipsum dolor');
+  $scope.products = [bistec, careta, chorizo];
 });
 
 var Product = function(title, image, description) {
